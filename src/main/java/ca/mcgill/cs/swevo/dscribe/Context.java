61604e58f348ed5549de1261c3dff74d87352329
AlexaHernandez <alexacarinahernandez@gmail.com>
2021-09-10 14:20:14 -0400

    Feat: add ability to configure src and bin folders

--- src/main/java/ca/mcgill/cs/swevo/dscribe/Context.java
+++ src/main/java/ca/mcgill/cs/swevo/dscribe/Context.java
@@ -1,79 +1,110 @@
 /*******************************************************************************
  * Copyright 2020 McGill University
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  *******************************************************************************/
 package ca.mcgill.cs.swevo.dscribe;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+
 import ca.mcgill.cs.swevo.dscribe.template.InMemoryTemplateRepository;
 import ca.mcgill.cs.swevo.dscribe.template.TemplateRepository;
 
 /**
- * Represents the context of execution of DScribe. It contains user settings such as the template
- * repository location.
+ * Represents the context of execution of DScribe. It contains user settings such as the template repository location.
+ * Implemented as a Singleton.
  * 
  * @author mnassif
  *
  */
-public class Context {
-  public enum TestClassNameConvention {
-    POSTFIX, PREFIX
-  };
+public class Context
+{
+	/* START: Default context variables */
+	private String templateRepositoryPath = defaultTemplateRepositoryPath();
+	private ClassLoader classLoader = Context.class.getClassLoader();
+	private final TestClassNameConvention testClassNameConvention = TestClassNameConvention.PREFIX;
+	/* END: Default context variables */
 
-  private static final Context INSTANCE = new Context();
-  private static final String ROOT = Paths.get("..").toAbsolutePath().normalize().toString();
-  private String templateRepoPath = Path.of("templates").toString(); // default location of
-                                                                     // templates
-  private ClassLoader classLoader = Context.class.getClassLoader(); // default class loader
+	private static final Context INSTANCE = new Context();
 
-  private TestClassNameConvention testClassNameConvention = TestClassNameConvention.POSTFIX;
+	public enum TestClassNameConvention
+	{
+		POSTFIX, PREFIX
+	};
 
-  private Context() {}
+	private Context()
+	{
+	}
 
-  public static Context instance() {
-    return INSTANCE;
-  }
+	protected static Context instance()
+	{
+		return INSTANCE;
+	}
 
-  // TO DO: Edit
-  public List<Path> srcPaths() {
-    return List.of(Path.of(ROOT, "JetUML", "src", "ca", "mcgill", "cs", "jetuml", "geom"),
-        Path.of(ROOT, "JetUML", "src", "ca", "mcgill", "cs", "jetuml", "diagram"));
-  }
+	// TODO: Edit
+	public List<Path> srcPaths()
+	{
+		String root = Paths.get("..").toAbsolutePath().normalize().toString();
+		return List.of(Path.of(root, "JetUML", "src", "ca", "mcgill", "cs", "jetuml", "geom"),
+				Path.of(root, "JetUML", "src", "ca", "mcgill", "cs", "jetuml", "diagram"));
+	}
 
-  public String templateRepositoryPath() {
-    return templateRepoPath;
-  }
+	public String templateRepositoryPath()
+	{
+		return templateRepositoryPath;
+	}
 
-  public void setTemplateRepositoryPath(String templateRepositoryPath) {
-    assert templateRepositoryPath != null;
-    this.templateRepoPath = templateRepositoryPath;
-  }
+	public void setTemplateRepositoryPath(String path)
+	{
+		templateRepositoryPath = path;
+	}
 
-  public ClassLoader classLoader() {
-    return classLoader;
-  }
+	public ClassLoader classLoader()
+	{
+		return classLoader;
+	}
 
-  public void setClassLoader(ClassLoader classLoader) {
-    assert classLoader != null;
-    this.classLoader = classLoader;
-  }
+	public void setClassLoader(ClassLoader loader)
+	{
+		classLoader = loader;
+	}
 
-  public TestClassNameConvention testClassNameConvention() {
-    return testClassNameConvention;
-  }
+	public TestClassNameConvention testClassNameConvention()
+	{
+		return testClassNameConvention;
+	}
 
-  public TemplateRepository templateRepository() {
-    return new InMemoryTemplateRepository(templateRepositoryPath());
-  }
+	public TemplateRepository templateRepository()
+	{
+		return new InMemoryTemplateRepository(templateRepositoryPath());
+	}
+
+	public String sourceFolder()
+	{
+		return "src";
+	}
+
+	public String testFolder()
+	{
+		return "test";
+	}
+
+	public String binFolder()
+	{
+		return Path.of("bin", "jetuml").toString();
+	}
+
+	private static String defaultTemplateRepositoryPath()
+	{
+		return Path.of("templates").toString();
+	}
 }
